68593421c5e5deab3c75753199976761b33af5a2
Olga <ptichan@inbox.ru>
2026-04-20 22:07:31 +0300

    функция для решения квадратного уравнения

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import math
-a = -3
-b = 8
-c = 11
-d = b**2-4*a*c
-r = ''
-if d < 0:
-    r = 'действительных корней нет'
-else:
-    x1 = (-b-math.sqrt(d))/(2*a)
-    x2 = (-b+math.sqrt(d))/(2*a)
-    r = 'x1='+str(round(x1, 3))+'\nx2='+str(round(x2, 3))
-print(r)
+
+
+def qvadra(a, b, c):
+    d = b**2-4*a*c
+    r = ''
+    if d < 0:
+        r = 'действительных корней нет'
+    else:
+        x1 = (-b-math.sqrt(d))/(2*a)
+        x2 = (-b+math.sqrt(d))/(2*a)
+        r = '\nx1='+str(round(x1, 3))+'\nx2='+str(round(x2, 3))
+    return (r)
+
+
+r1 = float(input('Введите a: '))
+r2 = float(input('Введите b: '))
+r3 = float(input('Введите c: '))
+
+print('Решение: ', qvadra(r1, r2, r3))
